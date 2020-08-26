1ccdc30cfdb312b8785e9ff0dd4469dfc2f91d1d
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-08-25 19:18:47 -0500

    overholder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Cart from './components/cart/Cart'
+
 
 function App() {
+
+  let [state, setState] = useState({
+    show: true
+  })
+
+  function toggleShow(){
+    setState({...state, show: !state.show})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <button onClick={toggleShow}>
+      Carrito de compras
+      </button>
+      
+      <Cart 
+      onCancel={toggleShow}
+      show={state.show}/>
+      
     </div>
   );
 }
